4248e62aa7ede78a3ce34d8fd5a9f495d696961a
innocentmaweta-hub <innocentmaweta@gmail.com>
2026-08-08 04:15:55 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /**
  * @license
  * SPDX-License-Identifier: Apache-2.0
- */
+ */ 
 
 import React, { useState, useRef, useEffect, useCallback } from 'react';
 import { Sidebar } from './components/Sidebar';
