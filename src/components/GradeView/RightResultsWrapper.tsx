5a3150414e98c255ed9235dcf904c18ff3aa1b97
innocentmaweta11-hub <innomaweta11@outlook.com>
2026-08-21 02:39:02 +0200

    feat(split): add split App + GradeView helpers and small components

---
+++ src/components/GradeView/RightResultsWrapper.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function RightResultsWrapper(props: any) {
+  return (
+    <div className="right-results-wrapper">
+      {/* Minimal results wrapper */}
+      <div>Results</div>
+    </div>
+  );
+}
+
+export default RightResultsWrapper;
