9e9d51c4218705b3a173511720223177891f073f
innocentmaweta-hub <innocentmaweta@gmail.com>
2026-08-28 00:56:54 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -360,7 +360,7 @@ export default function App() {
     const [showOldSessionModal, setShowOldSessionModal] =
         useState(false);
     const [pendingGradeNavigation, setPendingGradeNavigation] =
-        useRef(false);
+        useState(false);
 
     const [showNewCourseModal, setShowNewCourseModal] =
         useState(false);
